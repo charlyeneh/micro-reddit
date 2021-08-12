7810eeb3a25676f90576c9d475f320afa9b38026
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2021-08-12 15:44:47 +0100

    add validations to post model and runs tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-
+  
   validates :topic, presence: true, length: { minimum: 10 }
   validates :article, presence: true, length: { minimum: 20 }
 end
